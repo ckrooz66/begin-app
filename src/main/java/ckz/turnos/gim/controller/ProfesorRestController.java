608c75c931a53b944901e5447135d42a627d1e94
ckrooz66 <64717079+ckrooz66@users.noreply.github.com>
2022-10-30 20:08:21 -0300

    Agregar CORS
    Se agrega regla de cors

--- src/main/java/ckz/turnos/gim/controller/ProfesorRestController.java
+++ src/main/java/ckz/turnos/gim/controller/ProfesorRestController.java
@@ -7,11 +7,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,7 @@ import ckz.turnos.gim.entity.Profesor;
 import ckz.turnos.gim.service.IProfesorService;
 
 @RestController
+@CrossOrigin(origins = "*", methods= { RequestMethod.GET,RequestMethod.POST})
 @RequestMapping("/api")
 public class ProfesorRestController {
 
--- src/main/java/ckz/turnos/gim/controller/TurnoController.java
+++ src/main/java/ckz/turnos/gim/controller/TurnoController.java
@@ -7,19 +7,21 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import ckz.turnos.gim.entity.Profesor;
 import ckz.turnos.gim.entity.Turno;
 import ckz.turnos.gim.service.ITurnoService;
 
 @RestController
+@CrossOrigin(origins = "*", methods= { RequestMethod.GET,RequestMethod.POST})
 @RequestMapping("/turno")
 public class TurnoController {
 	
